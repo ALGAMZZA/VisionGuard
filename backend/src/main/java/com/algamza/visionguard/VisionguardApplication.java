fd82df6dce3bd329e8fd8238f7e084a31ed3c14e
SangHyunPark1213 <hjsh1213@naver.com>
2026-09-01 10:16:46 +0900

    프로젝트 뼈대 생성

---
+++ backend/src/main/java/com/algamza/visionguard/VisionguardApplication.java
@@ -0,0 +1,13 @@
+package com.algamza.visionguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisionguardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisionguardApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/algamza/visionguard/VisionguardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algamza.visionguard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisionguardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
